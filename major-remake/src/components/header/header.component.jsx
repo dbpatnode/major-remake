71e04e571ae141ccd479226bd2eb96e8d375ef5c
dbpatnodee <dbpatnode@hotmail.com>
2021-08-06 16:11:58 -0700

    aligned header options correctly

--- major-remake/src/components/header/header.component.jsx
+++ major-remake/src/components/header/header.component.jsx
@@ -3,10 +3,25 @@ import { Link } from 'react-router-dom';
 import './header.style.scss';
 
 const Header = () => (
-    <div>
-        <Link to="/" className="logo-container">
+    <div className="header">
+
+        <Link to="/" className="home-container">
             HOME
         </Link>
+        <div className='options'>
+            <Link to="/about" className="option">
+                ABOUT
+            </Link>
+            <Link to="/resume" className="option">
+                RESUME
+            </Link>
+            <Link to="/projects" className="option">
+                PROJECTS
+            </Link>
+            <Link to="/contact" className="option">
+                CONTACT
+            </Link>
+        </div>
     </div>
 )
 
