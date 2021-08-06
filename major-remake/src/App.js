133105ed64f98db92520b24e3c7c5dd587146e91
dbpatnodee <dbpatnode@hotmail.com>
2021-08-06 15:48:37 -0700

    homepage component set up

--- major-remake/src/App.js
+++ major-remake/src/App.js
@@ -1,10 +1,15 @@
 import './App.scss';
+import { Switch, Route } from 'react-router-dom';
 import Header from '../src/components/header/header.component'
+import HomePage from '../src/components/homepage/homepage.component'
 
 function App() {
   return (
     <div className="App">
-      <Header/>      
+      <Header/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+      </Switch>      
     </div>
   );
 }
---
+++ major-remake/src/components/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './homepage.style.scss';
+
+const HomePage = () => (
+    <div>
+        HomePage
+    </div>
+)
+
+export default HomePage
