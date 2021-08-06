8bb0c5a104eb1e59dc262d0c5823ef7b79c8a092
dbpatnodee <dbpatnode@hotmail.com>
2021-08-06 14:24:11 -0700

    most of the data set up

---
+++ major-remake/src/resumeData.js
@@ -0,0 +1,145 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Daniel Patnode",
+    "role": "Frontend Engineer",
+    "linkedinId":"daniel-patnode",
+    // "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles and keep active in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/daniel-patnode/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/dbpatnode",
+          "className":"fa fa-github"
+        },
+        
+        {
+          "name":"medium",
+          "url":"https://danielpatnode.medium.com/",
+          "className":"fa fa-medium"
+        }
+      ],
+    "aboutme":"Frontend Engineer with experience in Javascript based programming including multiple frameworks and libraries. My background in scientific glass blowing and organic agriculture contributed heavily to my joy of learning. I bring strong team-building, project management and interpersonal communication skills to the table that would help any sized team progress in a constructive and positive fashion. In my spare time I enjoy camping, getting outdoors, practicing Brazilian jiu-jitsu, exercising, checking out breweries and hunting for the perfect cheeseburger.",
+    "address":"Seattle, WA",
+    "website":"https://github.com/dbpatnode",
+    "education":[
+      {
+        "UniversityName":"Washington State University",
+        "specialization":"Bachelor of Science in Agricultural and Food Sciences",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2014",
+        "Achievements":"Founding member of the Organic Agriculture club."
+      },
+      {
+        "UniversityName":"Flatiron",
+        "specialization":"Full Stack Web Development",
+        "MonthOfPassing":"November",
+        "YearOfPassing":"2020",
+        "Achievements":"Ruby and Javascript based programming."
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Unearth Labs",
+        "specialization":"Front End Engineer Intern",
+        "MonthOfLeaving":"May",
+        "YearOfLeaving":"2021",
+        "Achievements":"Negated use of older versions of an internal data management platform, which reduced engineering costs and training overhead. Created a cohesive data management tool with MithrilJS, SCSS that enabled the customer service team to fulfill all of their account management needs without workarounds or requests to the engineering team. Applied changes that led to an overall decrease in load time across the platform by 95.8%, with requests to the API taking no longer than 5 seconds to complete in most instances."
+      },
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"SCSS/SASS"
+      },
+      {
+        "skillname":"JSON"
+      },
+      {
+        "skillname":"GitHub"
+      },
+      {
+        "skillname":"Git"
+      },
+      {
+        "skillname":"Ruby"
+      },
+      {
+        "skillname":"Ruby on Rails"
+      },
+      {
+        "skillname":"Restful APIs"
+      },
+      {
+        "skillname":"RPC Protocol"
+      },
+      {
+        "skillname":"MVC pattern"
+      },
+      {
+        "skillname":"PostgreSQL"
+      },
+      {
+        "skillname":"SQLite3"
+      },
+      {
+        "skillname":"Babel"
+      },
+      {
+        "skillname":"Webpack"
+      },
+      {
+        "skillname":"Chai"
+      },      
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
