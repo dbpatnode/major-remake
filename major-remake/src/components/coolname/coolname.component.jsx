83af49960bfe0ccbe9a16ec9459afa5b960e761c
dbpatnodee <dbpatnode@hotmail.com>
2021-08-07 15:34:08 -0700

    cool name partialy working

---
+++ major-remake/src/components/coolname/coolname.component.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import './coolname.style.scss'
+
+const CoolName = () => (
+    <div className="CoolName">
+        <ul>
+            <li>
+                <input type="checkbox" />
+                <div>D</div>
+            </li>
+            <li>
+                <input type="checkbox" />
+                <div>A</div>
+            </li>
+            <li>
+                <input type="checkbox" />
+                <div>N</div>
+            </li>
+            <li>
+                <input type="checkbox" />
+                <div>I</div>
+            </li>
+            <li>
+                <input type="checkbox" />
+                <div>E</div>
+            </li>
+            <li>
+                <input type="checkbox" />
+                <div>L</div>
+            </li>
+            {/* <li>
+                <input type="checkbox" />
+                <div>-</div>
+            </li>
+            <li>
+                <input type="checkbox" />
+                <div>P</div>
+            </li>
+            <li>
+                <input type="checkbox" />
+                <div>A</div>
+            </li>
+            <li>
+                <input type="checkbox" />
+                <div>T</div>
+            </li>
+            <li>
+                <input type="checkbox" />
+                <div>N</div>
+            </li>
+            <li>
+                <input type="checkbox" />
+                <div>O</div>
+            </li>
+            <li>
+                <input type="checkbox" />
+                <div>D</div>
+            </li>
+            <li>
+                <input type="checkbox" />
+                <div>E</div>
+            </li> */}
+        </ul>
+    </div>
+)
+
+export default CoolName
--- major-remake/src/components/homepage/homepage.component.jsx
+++ major-remake/src/components/homepage/homepage.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './homepage.style.scss';
 import resumeData from '../../resumeData';
+import CoolName from '../coolname/coolname.component'
 
 const HomePage = () => (
     <div>
+        <CoolName />
         {console.log(resumeData.name)}
         HomePage
     </div>
