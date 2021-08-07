aefa6091a9ecfc09c400b9df2765f6248bfbefa4
dbpatnodee <dbpatnode@hotmail.com>
2021-08-07 15:13:59 -0700

    getting resume data in homepage

--- major-remake/src/App.js
+++ major-remake/src/App.js
@@ -1,17 +1,26 @@
+import React from 'react';
 import './App.scss';
 import { Switch, Route } from 'react-router-dom';
 import Header from '../src/components/header/header.component'
 import HomePage from '../src/components/homepage/homepage.component'
 
-function App() {
+class App extends React.Component {
+render() {
   return (
     <div className="App">
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={HomePage}/>
-      </Switch>      
-    </div>
-  );
+        <Header/>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+
+          {/* <Route path='/about' component={HomePage} resumeData={resumeData}/> */}
+          {/* <Route path='/resume' component={HomePage} resumeData={resumeData}/>
+          <Route path='/projects' component={HomePage} resumeData={resumeData}/>
+          <Route path='/contact' component={HomePage} resumeData={resumeData}/> */}
+          
+        </Switch>      
+      </div>
+    );
+  }
 }
 
 export default App;
--- major-remake/src/components/homepage/homepage.component.jsx
+++ major-remake/src/components/homepage/homepage.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import './homepage.style.scss';
+import resumeData from '../../resumeData';
 
 const HomePage = () => (
     <div>
+        {console.log(resumeData.name)}
         HomePage
     </div>
 )
