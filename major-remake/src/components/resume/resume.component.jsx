fcb1702904927abb269cbbf3b001af8cab00aec4
dbpatnodee <dbpatnode@hotmail.com>
2021-08-13 11:08:28 -0700

    ?

--- major-remake/src/components/resume/resume.component.jsx
+++ major-remake/src/components/resume/resume.component.jsx
@@ -44,7 +44,7 @@ export default class Resume extends Component {
                                 return (
                                     <div className="row item">
                                         <div className="twelve columns">
-                                            <h3>{item.CompanyName}</h3>
+                                            <h3><a href={item.CompanyUrl}>{item.CompanyName}</a></h3>
                                             <p className="info">
                                                 {item.specialization}
                                                 <span>&bull;</span> <em className="date">{item.MonthOfLeaving} {item.YearOfLeaving}</em></p>
