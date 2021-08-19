03c213dd9f2013b4345bfe132817094b271bb95c
dbpatnodee <dbpatnode@hotmail.com>
2021-08-19 16:08:17 -0700

    fades on hover

--- major-remake/src/components/projectitem/project-item.component.jsx
+++ major-remake/src/components/projectitem/project-item.component.jsx
@@ -11,10 +11,10 @@ const CollectionItem = ({ name, imgUrl, description }) => (
             }}
         >
         </div>
+        <h1 className="name">{name}</h1>
+        <a className="button" id="github-button"><i className="fa fa-github" /></a>
+        <a className="button" id="demo-button"><i className="fa fa-link" /></a>
         <div className="collection-footer">
-            <h1 className="name">{name}</h1>
-            <a className="button" id="github-button"><i className="fa fa-github" /></a>
-            <a className="button" id="demo-button"><i className="fa fa-link" /></a>
             <span className="description">{description}</span>
         </div>
     </div>
