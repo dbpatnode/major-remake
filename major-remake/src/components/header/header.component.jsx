964bd6ca84ab45871b386fc9642551cc978b8337
dbpatnodee <dbpatnode@hotmail.com>
2021-11-11 10:12:55 -0800

    underlined buttons on header

--- major-remake/src/components/header/header.component.jsx
+++ major-remake/src/components/header/header.component.jsx
@@ -15,61 +15,51 @@ function Header() {
     return (
         <nav className="header">
 
-            {/* Left side buttons (disapear on mobile) */}
-            <div className="options">
+            {/* LOGO */}
+            <div className="logo">
                 <NavLink activeClassName="nav-active" to="/" className="option home-container">
-                    Daniel Patnode
+                    DP
                 </NavLink>
             </div>
 
-            <div className='options'>
-                <div className="right-side-wrap">
-                    <div className="web-options">
-                        <div className="web">
-                            <NavLink exact to="/projects" className="option">
-                                projects
-                            </NavLink>
-                        </div>
-                        <div className="web">
-                            <NavLink to="/blogs" className="option">
-                                blogs
-                            </NavLink>
-                        </div>
-                        <div className="web">
-                            <NavLink to="/resume" className="option">
-                                resume
-                            </NavLink>
-                        </div>
-                        <div className="web">
-                            <NavLink to="/contact" className="option">
-                                contact
-                            </NavLink>
-                        </div>
+
+            <div className="right-side-wrap">
+                {/* WEB BUTTONS */}
+                <div className="web-options">
+                    <div className="links">
+                        <NavLink exact to="/projects" className="option"> projects </NavLink>
+                        <NavLink to="/blogs" className="option"> blogs </NavLink>
+                        <NavLink to="/resume" className="option"> resume </NavLink>
+                        <NavLink to="/contact" className="option"> contact </NavLink>
                     </div>
-                    {/* right side buttons (hamburger not opened) */}
-                    <div class="hamburger-wrapper">
-                        <div className={isActive ? "navigation active" : "navigation"} >
-                            <div className="ham-btn" onClick={ToggleClass}>
-                                <span></span>
-                                <span></span>
-                                <span></span>
+                </div>
+
+
+                {/* MOBILE HAMBURGER*/}
+                <div class="hamburger-wrapper">
+                    <div className={isActive ? "navigation active" : "navigation"} >
+                        <div className="ham-btn" onClick={ToggleClass}>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+
+                        {/* HAMBURGER BUTTONS */}
+                        <div className="links">
+                            <div className="link" onClick={ToggleClass}>
+                                <NavLink activeClassName="nav-active" exact to="/">Home</NavLink>
                             </div>
-                            <div className="links">
-                                <div className="link" onClick={ToggleClass}>
-                                    <NavLink activeClassName="nav-active" exact to="/">Home</NavLink>
-                                </div>
-                                <div className="link" onClick={ToggleClass}>
-                                    <NavLink activeClassName="nav-active" to="/projects">Projects</NavLink>
-                                </div>
-                                <div className="link" onClick={ToggleClass}>
-                                    <NavLink activeClassName="nav-active" to="/blogs">Blogs</NavLink>
-                                </div>
-                                <div className="link" onClick={ToggleClass}>
-                                    <NavLink activeClassName="nav-active" to="/resume" >Resume</NavLink>
-                                </div>
-                                <div className="link" onClick={ToggleClass}>
-                                    <NavLink activeClassName="nav-active" to="/contact">contact</NavLink>
-                                </div>
+                            <div className="link" onClick={ToggleClass}>
+                                <NavLink activeClassName="nav-active" to="/projects">Projects</NavLink>
+                            </div>
+                            <div className="link" onClick={ToggleClass}>
+                                <NavLink activeClassName="nav-active" to="/blogs">Blogs</NavLink>
+                            </div>
+                            <div className="link" onClick={ToggleClass}>
+                                <NavLink activeClassName="nav-active" to="/resume" >Resume</NavLink>
+                            </div>
+                            <div className="link" onClick={ToggleClass}>
+                                <NavLink activeClassName="nav-active" to="/contact">contact</NavLink>
                             </div>
                         </div>
                     </div>
